bee6e4775e2a61377882dc05a6226cda32455414
Taewon Kim <kimtae9217@gmai.com>
2024-07-24 17:22:09 +0900

    배열1-연습문제3

---
+++ 정올/LC_C언어(단계별 문제)/array1_9067.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main ()
+{
+    int arr[10];
+
+    for(int i = 0; i < 10; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    printf("%d %d %d", arr[2], arr[4], arr[9]);
+    return 0;
+}
